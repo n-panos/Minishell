a8f2a066cf072cc297ec9b34f37940623f07c0ec
n-panos <45368267+n-panos@users.noreply.github.com>
2023-10-19 12:03:50 +0200

    Add files via upload

--- header/eminishell.h
+++ header/eminishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   eminishell.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ipanos-o <ipanos-o@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nacho <nacho@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 11:37:22 by ipanos-o          #+#    #+#             */
-/*   Updated: 2023/10/17 13:13:48 by ipanos-o         ###   ########.fr       */
+/*   Updated: 2023/10/19 12:01:33 by nacho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,14 +60,46 @@ typedef struct s_tokens
 	struct s_tokens		*prev;
 }	t_tokens;
 
+//		Funciones temporales para testeo en builtins.c
+
+int		ft_builtins(t_mini *mini, char *prompt);
+
 //      HAND CRAFTED FUNCTIONS -- BUILTINS
 
-int		ft_builtins(t_prg *prg, char *prompt);
-int		ft_exec_cd(char **cmd_mtx);
-int		ft_exec_echo(char **cmd_mtx);
-int		ft_exec_env(char **env, char **cmd_mtx);
-int		ft_exec_pwd(void);
-int		ft_exec_export(char **env);
-int		ft_exec_unset(t_prg *prg, char *str);
+int		ft_builtin_check(t_exec *exec, char **env);
+int		ft_cd(char **cmd_mtx);
+int		ft_echo(char **cmd_mtx);
+int		ft_env(char **env, char **cmd_mtx);
+int		ft_exit(char **cmd_mtx);
+int		ft_pwd(void);
+int		ft_export(char **env);
+int		ft_unset(t_prg *prg, char *str);
+
+//		SOLO FTS
+
+int		ft_preprocess_solo(t_mini *mini);
+pid_t	*ft_exec_solo(char **env, t_exec *exec);
+t_exec	*ft_init_exec(t_tokens *token, char **env);
+void	ft_in_out_type(t_tokens *token, t_exec *exec);
+
+//		PIPE FTS
+
+int		ft_preprocess_pipe(t_mini *mini);
+void	ft_exec_pipe(t_pipe *pipes, t_mini *mini);
+t_pipe	*ft_pipe_init(int pipe_num);
+void	ft_in_out_pipe(t_exec *exec, t_tokens *token);
+void	ft_in_out_default(t_pipe *pipes, int pipe_num);
+
+//		UTILS FTS
+
+int		here_doc(char *limiter);
+void	ft_error_cmd(char *str);
+char	*ft_find_path(char **envp, char *cmd);
+char	*ft_no_path(char *cmd, char **pos_paths);
+
+//		FREE FTS
+
+void	ft_free_pipes(t_pipe *pipes, int pipe_n);
+void	ft_free_exec(t_exec *exec);
 
 #endif
